2cd5f8ac2607f35809ebfb292d76197b48f607c3
Little-He-Guan <53397751+Little-He-Guan@users.noreply.github.com>
2023-11-13 16:41:02 +0800

    Add the home button to Yihui's part

--- app/src/main/java/com/can301/gp/demos/FrameAnimationsExample.java
+++ app/src/main/java/com/can301/gp/demos/FrameAnimationsExample.java
@@ -19,6 +19,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.can301.gp.Demonstration;
+import com.can301.gp.MainActivity;
 import com.can301.gp.R;
 import com.can301.gp.codepage.CodePage;
 
@@ -69,6 +70,14 @@ public class FrameAnimationsExample extends AppCompatActivity {
             Dialog diag = builder.create();
             diag.show();
         }
+    }
+
+    /**
+     * Go home when the user clicks the home button.
+     * @param view
+     */
+    public void goHome(View view) {
+        MainActivity.goBackToHomePage(this);
     }
 
 // Don't change END
